cd6e4ba7800fad5a397f1e9366c303ceb5e79aa5
DocPrior <Sara Prior>
2017-06-01 16:45:56 -0400

    link contacts and index view pages through crm server

--- crm.rb
+++ crm.rb
@@ -1,7 +1,16 @@
 require_relative 'contact'
 require 'sinatra'
 
-
 get ('/') do
   erb(:index)
+end
+
+get ('/contacts') do
+  @contacts = Contact.all
+  erb(:contacts)
+end
+
+
+after do
+  ActiveRecord::Base.connection.close
 end
