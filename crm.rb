ba3f2b50185114489af332ae3db4570b0454ff1a
DocPrior <Sara Prior>
2017-06-05 13:32:15 -0400

    connect new.erb page, change add contact link to point to new conact page

--- crm.rb
+++ crm.rb
@@ -15,14 +15,15 @@ get ('/about') do
   erb(:about)
 end
 
+get ('/contacts/new') do
+  erb(:new)
+end
+
 get ('/contacts/:id') do
   @contact = Contact.find(params[:id].to_i)
-  if @contact
     erb(:show_contact)
-  else
-    raise Sinatra::NotFound
-  end
 end
+
 
 after do
   ActiveRecord::Base.connection.close
